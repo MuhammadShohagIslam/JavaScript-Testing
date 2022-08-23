0edfc9ac84a005f08a123e408e48f056aa70566e
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-23 18:12:37 +0300

    Done Unit Testing: What & Why?

---
+++ introduction/introduction.js
@@ -0,0 +1,41 @@
+/*
+    *** JavaScript Testing ***
+        => It's typicaly means that we want to verify whether something
+        works as intended.
+        => When writing software or building websites, then we
+         typically want to test whether that software or that website 
+         we are building is working the way we expect it to work.
+*/
+
+/*
+    *** Two way test of out Software ***
+        --- Manual Testing ---
+            => Tedious and cumbersome
+            => Error-prone
+            => Often incomplete (not all scenarios covered)
+
+        --- Automated Testing ---
+            => Write your code then test your code
+            => inital effort (write tests), no effort thereafter
+            => Predictable and consistent
+            => High / complete code and scenario coverage can achieved.
+*/
+
+/*
+        *** Different kind of Automate Testing ***
+                --- Unit Testing ---
+            => A building block of our app
+            => Ideally, the smallest possibile building block
+            => eg. function, a class, a component.
+
+        *Application made up with all of the Unit*
+            => App = combination of all units
+                => If all units were tested, the overall app should work -> Backed up by integration tests
+                => changes are always tested against all units to avoid bugs.
+
+        *** Why Unit Testing ***
+            => Avoids endless amount of manual testing
+            => Allows we to cover(close to) 100% of our code and scenarios
+            => code changes are tested against all scenarios almost instantly
+            => Write cleaner and better code (beacuse testing then becomes easier)
+*/
