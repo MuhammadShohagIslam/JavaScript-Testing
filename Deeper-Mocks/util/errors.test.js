a3964bf352c839f8461ce2ec3a4a9d6a67f72150
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-29 06:33:34 +0300

    Done Error file Testing

---
+++ Deeper-Mocks/util/errors.test.js
@@ -0,0 +1,43 @@
+import { it, describe, expect } from "vitest";
+import { HttpError, ValidationError } from "./errors";
+
+describe("HttpError Class", () => {
+    it("should contain provided statusCode, message and data", () => {
+        const statusCode = 1;
+        const message = "test";
+        const data = { key: "data text" };
+
+        const httpError = new HttpError(statusCode, message, data);
+
+        expect(httpError.statusCode).toBe(statusCode);
+        expect(httpError.message).toBe(message);
+        expect(httpError.data).toBe(data);
+    });
+
+    it("should contain undefined as data if not provided data", () => {
+        const statusCode = 1;
+        const message = "test";
+
+        const httpError = new HttpError(statusCode, message);
+
+        expect(httpError.statusCode).toBe(statusCode);
+        expect(httpError.message).toBe(message);
+        expect(httpError.data).toBeUndefined();
+    });
+});
+
+describe("ValidationError Class", () => {
+    it("should contain provided message", () => {
+        const message = "abc";
+
+        const validationError = new ValidationError(message);
+
+        expect(validationError.message).toBe(message);
+    });
+
+    it("should contain undefined as message  if not provided", () => {
+        const validationError = new ValidationError();
+
+        expect(validationError.message).not.toBeDefined();
+    });
+});
